dae4ebfdd62a616ffff9441c575ea4575f07da5d
lorddark2358-max <lorddark2358@gmail.com>
2026-05-30 18:05:22 -0500

    Initial commit - configuration and root files

---
+++ capacitor.config.ts
@@ -0,0 +1,7 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hybridpb.app',
+  appName: 'Combat Strava',
+  webDir: 'dist'
+};\n\nexport default config;
